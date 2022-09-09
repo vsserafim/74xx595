7bb062a5e6a39530bb6711f74459becdce78c40a
Vinicius da Silveira Serafim <vinicius@serafim.eti.br>
2022-09-09 10:08:52 -0300

    Removed unused MCUs profiles.

--- include/mcu_setup.h
+++ include/mcu_setup.h
@@ -9,16 +9,6 @@
 
 #ifdef __AVR_ATmega328P__
 #include "mcu_setup/atmega328p.h"
-
-#elif __AVR_ATmega8__
-#include "mcu_setup/atmega8.h"
-
-#elif __AVR_ATtiny85__
-#include "mcu_setup/attiny85.h"
-
-#elif __AVR_ATtiny13A__
-#include "mcu_setup/attiny13a.h"
-
 #endif
 
 #endif // MCU_SETUP_H
